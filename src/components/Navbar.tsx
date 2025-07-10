eb52ad15377b36e68d0679f83337425404261642
BeyonderxX <alidelhiindia@gmail.com>
2025-07-10 21:09:05 +0530

    Changes in UI

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,28 @@
 // src/components/Navbar.tsx
+"use client"
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+  const pathname = usePathname();
   return (
     <nav className="bg-[#EEEFE0] p-4 flex gap-4 justify-center">
-      <Link href="/" className="font-bold hover:text-[#A7C1A8]">
+      <Link
+        href="/"
+        className={`${pathname === '/' ? 'font-bold' : ''} hover:text-[#A7C1A8]`}
+      >
         Home
       </Link>
-      <Link href="/about" className="hover:text-[#A7C1A8]">
+      <Link
+        href="/about"
+        className={`${pathname === '/about' ? 'font-bold' : ''} hover:text-[#A7C1A8]`}
+      >
         About
       </Link>
-      <Link href="/contact" className="hover:text-[#A7C1A8]">
+      <Link
+        href="/contact"
+        className={`${pathname === '/contact' ? 'font-bold' : ''} hover:text-[#A7C1A8]`}
+      >
         Contact
       </Link>
     </nav>
